dfa43c3fa85646bd93438b66b1a1459fbca99dd4
parksangmoon <cholsoo22001@gmail.com>
2016-05-30 20:38:27 +0900

    완성! 코멘트만 남음

--- Algorithm/lcs.py
+++ Algorithm/lcs.py
@@ -2,7 +2,6 @@
 # 2010-12343 박상문
 from math import floor
 
-
 '''
  in b, we have 3 choice.
  1 ->  left
@@ -84,6 +83,49 @@ def find_lcs_of_two(slice_a, slice_b):
     lcs_b = '_' * (len(slice_b) - len(lcs_b)) + lcs_b
     return lcs_a, lcs_b, length
 
+
+def find_all_lcs(gene_a, gene_b, name1, name2):
+    """Find_all lcs for first inputs."""
+    len_gene_a = len(gene_a)
+    len_gene_b = len(gene_b)
+    for i in range(floor(len_gene_a / 50)):
+        interval_a = 0
+        if len_gene_a - 50 * i <= 50:
+            break
+        elif 51 <= len_gene_a - 50 * i <= 100:
+            interval_a = len_gene_a - 50 * i
+        else:
+            interval_a = 100
+
+        max_a = ''
+        max_b = ''
+        idx_a = 0
+        idx_b = 0
+        max_length = 0
+        slice_a = gene_a[50 * i: 50 * i + interval_a]
+
+        for j in range(floor(len_gene_b / 50)):
+            interval_b = 0
+            if len_gene_b - 50 * j <= 50:
+                break
+            elif 51 <= len_gene_b - 50 * j <= 100:
+                interval_b = len_gene_b - 50 * j
+            else:
+                interval_b = 100
+
+            slice_b = gene_b[50 * j: 50 * j + interval_b]
+            lcs_a, lcs_b, length = find_lcs_of_two(slice_a, slice_b)
+            if (length > max_length):
+                max_a = lcs_a
+                max_b = lcs_b
+                idx_a = i
+                idx_b = j
+                max_length = length
+
+        print(name1 + str(idx_a * 50) + ',' + name2 + str(idx_b * 50))
+        print(max_a)
+        print(max_b)
+
 # main start
 file_A = open("geneA.fasta", 'r')
 file_B = open("geneB.fasta", 'r')
@@ -104,44 +146,8 @@ for line in lines_B:
     gene_B += line.strip()
 # 전처리 끝
 
-for i in range(floor(len(gene_A) / 50)):
-    interval_A = 0
-    if len(gene_A) - 50 * i <= 50:
-        break
-    elif 51 <= len(gene_A) - 50 * i <= 100:
-        interval_A = len(gene_A) - 50 * i
-    else:
-        interval_A = 100
-
-    max_A = ''
-    max_B = ''
-    idx_A = 0
-    idx_B = 0
-    max_length = 0
-    slice_A = gene_A[50 * i: 50 * i + interval_A]
-
-    for j in range(floor(len(gene_B) / 50)):
-        interval_B = 0
-        if len(gene_B) - 50 * j <= 50:
-            break
-        elif 51 <= len(gene_B) - 50 * j <= 100:
-            interval_B = len(gene_B) - 50 * j
-        else:
-            interval_B = 100
-
-        slice_B = gene_B[50 * j: 50 * j + interval_B]
-        lcs_a, lcs_b, length = find_lcs_of_two(slice_A, slice_B)
-        if (length > max_length):
-            max_A = lcs_a
-            max_B = lcs_b
-            idx_A = i
-            idx_B = j
-            max_length = length
-
-    print('A' + str(idx_A * 50) + ',B' + str(idx_B * 50))
-    print(max_A)
-    print(max_B)
-
+find_all_lcs(gene_A, gene_B, 'A', 'B')
+find_all_lcs(gene_B, gene_A, 'B', 'A')
 
 file_A.close()
 file_B.close()
